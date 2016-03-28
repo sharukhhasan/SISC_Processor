b48f8e99043ebec38745f48e5e789f434ab3a476
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-03-27 19:29:30 -0500

    initial files commit

---
+++ ctrl.v
@@ -0,0 +1,53 @@
+// ECE:3350 SISC computer project
+// finite state machine
+
+`timescale 1ns/100ps
+
+module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel, rd_sel);
+  
+  /* TODO: Declare the ports listed above as inputs or outputs */
+  
+  // states
+  parameter start0 = 0, start1 = 1, fetch = 2, decode = 3, execute = 4, mem = 5, writeback = 6;
+   
+  // opcodes
+  parameter NOOP = 0, LOD = 1, STR = 2, ALU_OP = 8, BRA = 4, BRR = 5, BNE = 6, HLT=15;
+	
+  // addressing modes
+  parameter am_imm = 8;
+
+  // state registers
+  reg [2:0]  present_state, next_state;
+
+  /* TODO: Write a clock process that progresses the fsm to the next state on the
+       positive edge of the clock, OR resets the state to 'start0' on the negative edge
+       of rst_f. Notice that the computer is reset when rst_f is low, not high. */
+
+
+  /* TODO: Write a process that determines the next state of the fsm. */
+
+
+  // Halt on HLT instruction
+  always @ (opcode)
+  begin
+    if (opcode == HLT)
+    begin 
+      #1 $display ("Halt."); //Delay 1 ns so $monitor will print the halt instruction
+      $stop;
+    end
+  end
+    
+  /* TODO: Generate outputs based on the FSM states and inputs. For Parts 2 and 3, you will
+       add the new control signals here. */
+
+  // fetch
+    
+  // decode
+
+  // execute
+    
+  // mem
+    
+  // write back
+
+endmodule
